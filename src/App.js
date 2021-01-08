bcd5bd0cc38dd491d1798b37089d6378212f6151
Guillermo Torres <gtorres.alonso@gmail.com>
2021-01-07 22:53:27 -0600

    Add react Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+      <Route exact path="/starred">
+        this is starred
+      </Route>
+      {/* Manage unexisting Routes */}
+      <Route>Not Found 404</Route>
+    </Switch>
+  );
 }
 
 export default App;
